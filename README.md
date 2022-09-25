0eef494a1b98d018054730030239627bb14152c4
Özgür Öğdem <ogdemster@gmail.com>
2022-09-25 14:28:40 +0300

    FirstCommit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UniSwap
+
+Excange Contract Deployed at:  0xa25D3cf598849fbA553D1e157718B7750e3FA093
---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  const cryptoDevTokenAddress = CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS;
+
+  const exchangeContract = await ethers.getContractFactory("Exchange");
+
+  const deployedExchangeContact = await exchangeContract.deploy(
+    cryptoDevTokenAddress
+  );
+  await deployedExchangeContact.deployed();
+  console.log(
+    "Excange Contract Deployed at: ",
+    deployedExchangeContact.address
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
